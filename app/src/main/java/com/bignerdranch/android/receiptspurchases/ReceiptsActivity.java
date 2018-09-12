91084e250547ebda56d84c81cd725416514b3f4e
Mitch <msr005@student.usc.edu.au>
2018-09-12 11:49:55 +1000

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/receiptspurchases/ReceiptsActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.receiptspurchases;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReceiptsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipts);
+    }
+}
